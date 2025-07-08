dfc9c0164cc64e58fff1723bbd609d50ff7f047a
lourd <z1309014381@gmail.com>
2025-07-08 11:23:52 +0800

    feat: Update application title and restructure main App component to include routing, localization, and authentication providers

--- apps/frontend/src/app.tsx
+++ apps/frontend/src/app.tsx
@@ -1,7 +1,25 @@
 import 'src/global.css';
+import { Router } from 'src/routes/sections';
+
+import { useScrollToTop } from 'src/hooks/use-scroll-to-top';
+import { LocalizationProvider } from 'src/locales';
+import { I18nProvider } from 'src/locales/i18n-provider';
+import { AuthProvider } from 'src/auth/context/jwt';
+import { MotionLazy } from 'src/components/animate/motion-lazy';
+
 
 // ----------------------------------------------------------------------
 
 export default function App() {
-  return <div>Hello World</div>;
+  useScrollToTop();
+
+  return <I18nProvider>
+    <LocalizationProvider>
+      <AuthProvider>
+        <MotionLazy>
+          <Router />
+        </MotionLazy>
+      </AuthProvider>
+    </LocalizationProvider>
+  </I18nProvider>;
 }
