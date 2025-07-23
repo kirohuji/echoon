01c6621be342bc8b737959cbd2aeadcda62b89bd
lourd <z1309014381@gmail.com>
2025-07-23 14:02:36 +0800

    feat: Add react-dropzone@14.3.8 to frontend and backend dependencies, update pnpm-lock.yaml, and remove unused mockConversations and file-thumbnail components for cleaner codebase

--- apps/frontend/src/components/hook-form/fields.tsx
+++ apps/frontend/src/components/hook-form/fields.tsx
@@ -1,8 +1,13 @@
 import { RHFTextField } from './rhf-text-field';
 import { RHFEditor } from './rhf-editor';
+import { RHFUpload, RHFUploadBox, RHFUploadAvatar } from './rhf-upload';
+
 // ----------------------------------------------------------------------
 
 export const Field = {
   Text: RHFTextField,
   Editor: RHFEditor,
+  UploadBox: RHFUploadBox,
+  Upload: RHFUpload,
+  UploadAvatar: RHFUploadAvatar,
 };
